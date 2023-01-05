9e5acfe4101644bee0751ad822b21bec6a780b28
diabhey <abhimanyuselvan@protonmail.com>
2023-01-05 21:21:17 +0100

    wip do infra

--- assets/terraform/digitalocean-project.tf
+++ assets/terraform/digitalocean-project.tf
@@ -1,9 +1,8 @@
 resource "digitalocean_project" "playground" {
-  name        = "mastodon"
+  name        = "twitter"
   description = "A project to run Mastodon on DigitalOcean."
   purpose     = "Web Application"
   environment = "Production"
   resources = [digitalocean_kubernetes_cluster.mastodon.urn, digitalocean_database_cluster.mastodon_pg[0].urn,
-    digitalocean_database_cluster.mastodon_redis[0].urn,
   digitalocean_spaces_bucket.mastodon_spaces[0].urn]
 }
--- assets/terraform/providers.tf
+++ assets/terraform/providers.tf
@@ -23,7 +23,7 @@ locals {
 }
 
 provider "digitalocean" {
-  token = var.do_token
+  token             = var.do_token
   spaces_access_id  = var.s3_bucket_access_key_id
   spaces_secret_key = var.s3_bucket_access_key_secret
   # Provider is configured using environment variables:
