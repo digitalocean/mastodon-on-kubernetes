d00e6edf7cdab5da3f77333fe92517eba7466b29
diabhey <abhimanyuselvan@protonmail.com>
2023-01-09 14:47:49 +0100

    chore: display terraform outputs

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,7 +1,15 @@
-# output "mastodon_helm_chart_values" {
-#   value = helm_release.mastodon.values
-# }
+output "cluster_id" {
+  value = digitalocean_kubernetes_cluster.mastodon.id
+}
 
-# output "mastodon_helm_chart_manifest" {
-#   value = helm_release.mastodon.manifest
-# }
+output "cluster_name" {
+  value = digitalocean_kubernetes_cluster.mastodon.name
+}
+
+output "argocd_helm_chart_values" {
+  value = helm_release.argocd[0].values
+}
+
+output "argocd_helm_chart_manifest" {
+  value = helm_release.argocd[0].manifest
+}
